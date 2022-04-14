7e0d5d771ba8ecfc31dba7d7726d12323aeb0f84
McNeelinet <lavelin.sava@gmail.com>
2022-04-14 21:03:24 +0300

    Добавил поддержку репостов
    Добавил поддержку репостов
    Картинка теперь выбирается в самом высоком разрешении
    
    Добавить поддержку видео не удалось. Если оно было выложено в плеере вк, то в тг не отобразится. Если вставить ссылку на видео, тогда отобразится.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import telebot
-from telebot.types import InputMediaPhoto, InputMedia
+from telebot.types import InputMediaPhoto
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
@@ -29,14 +29,16 @@ def tgsend_mediagroup(media):
     tgbot.send_media_group(tggroupid, media)
 
 
+# функция получает все
 def get_photos(attachments):
     photos = []
     for attachment in attachments:
         if attachment['type'] == 'photo':
             photo = attachment['photo']
-            for size in photo['sizes']:
-                if size['type'] == 'y':
+            for size in reversed(photo['sizes']):  # ищем максимальный размер фото
+                if size['type'] in ('s', 'm', 'x', 'y', 'z', 'w'):
                     photos.append(InputMediaPhoto(size['url']))
+                    break
     return photos
 
 
@@ -46,7 +48,12 @@ def listen():
             if event.type == VkBotEventType.WALL_POST_NEW:
                 post = event.obj
                 attachments = post.attachments
-                if attachments:
+                repost = post.copy_history
+                if repost:  # проверка на наличие репоста
+                    repost_link = f'https://vk.com/wall{repost[0]["from_id"]}_{repost[0]["id"]}'
+                else:
+                    repost_link = ''
+                if attachments:  # отправить сообщение с вложениями
                     # вложения по типам
                     photos = get_photos(attachments)
 
@@ -54,10 +61,10 @@ def listen():
                     if len(attachments) > 1:
                         media[-1].caption = post.text  # добавляет текст к последнему вложению
                         tgsend_mediagroup(media)
-                    elif len(photos) == 1:
-                        tgsend_photo(photos[0].media, {post.text})
-                elif post.text:
-                    tgsend_message(f'{post.text}')
+                    elif len(photos) == 1:  # если единственное вложение - фото(для этого случая нужен отдельный метод)
+                        tgsend_photo(photos[0].media, post.text + f'\n{repost_link}')
+                elif post.text or repost_link:  # отправить простое сообщение
+                    tgsend_message(f'{post.text}\n{repost_link}')
     except Exception:
         listen()
 
